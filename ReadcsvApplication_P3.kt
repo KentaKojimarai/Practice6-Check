9497bec9ee7820471f12d29c86d5afef18766534
Kenta Kojima <167041420+KentaKojimarai@users.noreply.github.com>
2024-05-14 15:09:55 +0900

    演習3 改良コード (logとディレクトリ移動機能追加)

---
+++ ReadcsvApplication_P3.kt
@@ -0,0 +1,29 @@
+package com.example.readcsv
+
+import org.springframework.boot.ApplicationRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+
+@EnableJpaRepositories(
+	basePackages = ["repository"]
+)
+@SpringBootApplication
+class ReadcsvApplication(
+	val productService: ProductService
+) {
+	val filePath = "C:/Users/10260226/product.csv"
+	val successDir = "C:/Users/10260226/finishedCSV/Success/"
+	val errorDir = "C:/Users/10260226/finishedCSV/error/"
+	@Bean
+	fun applicationRunner() =
+		ApplicationRunner {
+			productService.importCSVData(filePath, successDir, errorDir)
+		}
+}
+
+fun main(args: Array<String>) {
+	val run =runApplication<ReadcsvApplication>(*args)
+	run.close()
+}
